fc8633110bea94334732675a39ad6de4d71d4dfc
Class Account <cs294-ad@icluster13.EECS.Berkeley.EDU>
2013-03-02 19:18:44 -0800

    generates a roc plot for each fold

--- classifier.scala
+++ classifier.scala
@@ -11,6 +11,7 @@ import scala.util.Random
 run.main()
 
 class classifier(xTraining:ArrayBuffer[SMat], yTraining:ArrayBuffer[SMat], xTest:ArrayBuffer[SMat], yTest:ArrayBuffer[SMat], THRESHOLD:Float) {
+  var roc = plot()
   var myPlot = plot()
   var myPlot2 = plot()
   var numFeatures:Int = xTraining(0).nrows
@@ -79,6 +80,7 @@ class classifier(xTraining:ArrayBuffer[SMat], yTraining:ArrayBuffer[SMat], xTest
     val F1:Float = (2*precision*recall) / (precision + recall)
     val sensitivity:Float = tp / ( tp + fn )
     val specificity:Float = tn / ( fp + tn )
+    roc.addPoint(0, 1-specificity, sensitivity)
     myPlot.addPoint(0, iters-1, sensitivity, true)
     myPlot.addPoint(1, iters-1, 1-specificity, true)
     myPlot.addPoint(2, iters-1, specificity, true)
